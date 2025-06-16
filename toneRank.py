64806fbb1701711599530f2785b2f2ec651e03af
Rylan Ahmadi <rahmadi@ncsu.edu>
2025-06-16 11:19:59 -0400

    [Implementation] Finished the beta version of ToneRank. Full data flow now available.

--- toneRank.py
+++ toneRank.py
@@ -1,10 +1,11 @@
 # The top-level class for the ToneRank application
 # @author Rylan Ahmadi (Ry305)
-# Last updated 06/12/2025
+# Last updated 06/16/2025
 
 from gmailPipe import GmailPipe
 
 # A list of the top 100 public domain email addresses, accounting for approx. 75.83% of active emails
+# Source: email-verify.my-addr.com/list-of-most-popular-email-domains.php
 public_email_domains = {
     "gmail.com", "yahoo.com", "hotmail.com", "aol.com", "hotmail.co.uk", "hotmail.fr", 
     "msn.com", "yahoo.fr", "wanadoo.fr", "orange.fr", "comcast.net", "yahoo.co.uk", 
@@ -25,20 +26,76 @@ public_email_domains = {
     "bigpond.net.au"
 }
 
+def urgency_prompt(email, client):
+    """ Uses the GroqLlama class to prompt Llama3 to calculate an urgency score for a specific email. """
+
+    # Prompt with examples (potential bias)
+    prompt1 = "You are a personal assistant to an overworked government official. It is of vital importance " \
+    "that his most urgent emails are prioritized, because he might not be able to respond to all of them. " \
+    "He is now going to give you an email, and wants you to assign an Urgency Score from 0 to 10 (inclusive) " \
+    "to it, with 0 corresponding to an email which might not ever require a response, and 10 being an email " \
+    "which must be addressed immediately. Possible indicators of urgency could include: using all caps, an angry " \
+    "or frustrated tone, and words or phrases such as 'ASAP', 'as soon as possible', 'vital', 'action required', " \
+    "'immediately', 'cannot wait', and so on. " \
+    "Your response should contain NO text except an integer number which is no less than 0 and no more than 10.\n\n" \
+    "Email subject: " + email.subject + "\n" \
+    "Email body: " + email.body + "\n"
+
+    # Prompt without examples (no bias)
+    prompt2 = "You are a personal assistant to an overworked government official. It is of vital importance " \
+    "that his most urgent emails are prioritized, because he might not be able to respond to all of them. " \
+    "He is now going to give you an email, and wants you to assign an Urgency Score from 0 to 10 (inclusive) " \
+    "to it, with 0 corresponding to an email which might not ever require a response, and 10 being an email " \
+    "which must be addressed immediately. " \
+    "Your response should contain NO text except an integer number which is no less than 0 and no more than 10.\n\n" \
+    "Email subject: " + email.subject + "\n" \
+    "Email body: " + email.body + "\n"
+
+    # Prompt with examples (potential bias)
+    prompt3 = "You are a personal assistant to an overworked government official. It is of vital importance " \
+    "that his most urgent emails are prioritized, because he might not be able to respond to all of them. " \
+    "He is now going to give you an email, and wants you to assign an Urgency Score from 0 to 10 (inclusive) " \
+    "to it, with 0 corresponding to an email which might not ever require a response, and 10 being an email " \
+    "which must be addressed immediately. Possible indicators of urgency could include: using all caps, an angry " \
+    "or frustrated tone, and words or phrases such as 'ASAP', 'as soon as possible', 'vital', 'action required', " \
+    "'immediately', 'cannot wait', and so on. " \
+    "Your response should contain NO text except an decimal number of the format X.X which is no less than 0.0 " \
+    "and no more than 10.0.\n\n" \
+    "Email subject: " + email.subject + "\n" \
+    "Email body: " + email.body + "\n"
+
+    # The same as prompt2, but it asks for a decimal
+    prompt4 = "You are a personal assistant to an overworked government official. It is of vital importance " \
+    "that his most urgent emails are prioritized, because he might not be able to respond to all of them. " \
+    "He is now going to give you an email, and wants you to assign an Urgency Score from 0 to 10 (inclusive) " \
+    "to it, with 0 corresponding to an email which might not ever require a response, and 10 being an email " \
+    "which must be addressed immediately. " \
+    "Your response should contain NO text except an decimal number of the format X.X which is no less than 0.0 " \
+    "and no more than 10.0.\n\n" \
+    "Email subject: " + email.subject + "\n" \
+    "Email body: " + email.body + "\n"
+
+    response = GroqLlama.get_cached_llama_response(client, prompt3) # Query Llama3
+    return float(response) # Return uscore
+
 if __name__ == '__main__':
     emails = GmailPipe.get_emails_last_24_hours() # get emails
-    print(f"Found {len(emails)} messages from the past 24 hours.") # Print number of messages
 
     # split into their categories
     cat1_emails = []
     cat2_emails = []
     for e in emails:
         domain = e.sender.split("@")[1] # get the part of the sender data after the '@'
-        domain = domain[0:len(domain)-1] # remove the closing '>'
+        if (domain[len(domain) - 1] == '>'):
+            domain = domain[0:len(domain)-1] # remove the closing '>', if there is one
         if domain in public_email_domains: 
             cat2_emails.append(e) # If the email is a public domain
         else:
             cat1_emails.append(e) # If the email is NOT a public domain
+
+    # TODO: remove this line
+    # Print number of messages
+    print(f"Found {len(emails)} messages from the past 24 hours ({len(cat1_emails)} from C1, {len(cat2_emails)} from C2).")
 
     # Use llm.py to get a Llama3 client
     from llm import GroqLlama
@@ -46,6 +103,20 @@ if __name__ == '__main__':
 
     # TODO: calculate urgency score for each email
 
-    # TODO: rank each category by urgency
+    # Calculate urgency score for each email in category 1
+    for e in cat1_emails:
+        uscore = urgency_prompt(e, llama3) # get the urgency score using helper method
+        e.uscore = uscore # set uscore
+    # Calculate urgency score for each email in category 2
+    for e in cat2_emails:
+        uscore = urgency_prompt(e, llama3) # get the urgency score using helper method
+        e.uscore = uscore # set uscore
 
-    # TODO: merge categories back together
+    # Create overall email ranking
+    emails_ranked = sorted(cat1_emails) + sorted(cat2_emails)
+
+    # Print ranked list of emails (this serves as a prototype for the Priority Report)
+    count = 1
+    for e in emails_ranked:
+        print(f"{count}. {e}")
+        count = count + 1
